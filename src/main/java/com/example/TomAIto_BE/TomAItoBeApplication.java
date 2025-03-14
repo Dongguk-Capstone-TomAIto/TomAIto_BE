c1330877b19de20d16c8817fa522d8298ed13965
Dlrjswn <lkj6387@gmail.com>
2025-03-15 04:37:06 +0900

    first commit

---
+++ src/main/java/com/example/TomAIto_BE/TomAItoBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.TomAIto_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomAItoBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TomAItoBeApplication.class, args);
+	}
+
+}
