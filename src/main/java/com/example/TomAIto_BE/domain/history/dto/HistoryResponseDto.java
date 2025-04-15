be5334cdf0b3706354af36ddb2c0b0607009b93c
Dlrjswn <lkj6387@gmail.com>
2025-04-15 19:58:20 +0900

    HistoryResponseDto Getter 구현

--- src/main/java/com/example/TomAIto_BE/domain/history/dto/HistoryResponseDto.java
+++ src/main/java/com/example/TomAIto_BE/domain/history/dto/HistoryResponseDto.java
@@ -3,17 +3,20 @@ package com.example.TomAIto_BE.domain.history.dto;
 import com.example.TomAIto_BE.domain.history.entity.History;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
+import lombok.Getter;
 
 import java.time.LocalDate;
 import java.util.List;
 
 public class HistoryResponseDto {
     @Builder
+    @Getter
     public static class saveDto{
         private LocalDate createdAt;
         private String result;
     }
     @Builder
+    @Getter
     public static class HistoryDto {
         private LocalDate createdAt;
         private String result;
